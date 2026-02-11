4a71bc21d20777d9be930cd7b77cc712c776c10a
Maria Teplinskaya <pythagoreantree@googlemail.com>
2026-02-11 22:03:04 +0300

    [commit-4] добавила очередь на случай, если consumer стартанет первым

--- src/main/java/ru/post/PostRegistrationApp/config/RabbitMQConfig.java
+++ src/main/java/ru/post/PostRegistrationApp/config/RabbitMQConfig.java
@@ -1,11 +1,26 @@
 package ru.post.PostRegistrationApp.config;
 
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "post_created.queue";
+    public static final String DLX_EXCHANGE = "posts.dlx";
+    public static final String DLX_ROUTING_KEY = "post.created.dlq";
+
+    @Bean
+    public Queue postsQueue() {
+        return QueueBuilder.durable(QUEUE_NAME)
+                .withArgument("x-dead-letter-exchange", DLX_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", DLX_ROUTING_KEY)
+                .build();
+    }
 
     @Bean
     public JacksonJsonMessageConverter messageConverter() {
